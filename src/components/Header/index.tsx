c85714848f9e89e7d8ccddd526d830e132335091
Cesar Oliveira <cesarsdoliveira@gmail.com>
2023-07-12 23:46:04 -0300

    create: create nextjs project and first head component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,17 @@
+import styles from './styles.module.scss'
+import format from 'date-fns/format'
+import enGB from 'date-fns/locale/en-GB'
+
+export const Header = () => {
+  const currentDate = format(new Date(), 'EEEEEE, d MMMM', {
+    locale: enGB
+  })
+
+  return (
+    <header className={styles.container}>
+      <img src="/logo.svg" alt="Podcastr" />
+      <p>Listen to the best available here</p>
+      <span>{currentDate}</span>
+    </header>
+  )
+}
